cd66718ea39af0e04eceb3038b2240a02392388b
Sander Marechal <sander@prezent.nl>
2018-06-04 15:41:12 +0200

    Drop PHP7.1+ only test dependency

--- Tests/DependencyInjection/PrezentDoctrineTranslatableExtensionTest.php
+++ Tests/DependencyInjection/PrezentDoctrineTranslatableExtensionTest.php
@@ -3,9 +3,7 @@
 namespace Prezent\Doctrine\TranslatableBundle\Tests\DependencyInjection;
 
 use PHPUnit\Framework\TestCase;
-use PackageVersions\Versions;
 use Prezent\Doctrine\TranslatableBundle\DependencyInjection\PrezentDoctrineTranslatableExtension;
-use Composer\Semver\Semver;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class PrezentDoctrineTranslatableExtensionTest extends TestCase
@@ -30,9 +28,9 @@ class PrezentDoctrineTranslatableExtensionTest extends TestCase
 
     public function testSonataFilter()
     {
-        try {
-            $sonataVersion = Versions::getVersion('sonata-project/doctrine-orm-admin-bundle');
-        } catch (\OutOfBoundsException $e) {
+        $sonataVersion = getenv('SONATA');
+
+        if (!$sonataVersion) {
             $this->markTestSkipped('Sonata is not installed.');
             return;
         }
@@ -43,7 +41,7 @@ class PrezentDoctrineTranslatableExtensionTest extends TestCase
         $extension = new PrezentDoctrineTranslatableExtension();
         $extension->load([[]], $container);
 
-        if (Semver::satisfies($sonataVersion, '^3.0')) {
+        if ($sonataVersion == '^3.6') {
             $this->assertTrue($container->hasDefinition('prezent_doctrine_translatable.sonata.filter'));
         } else {
             $this->assertFalse($container->hasDefinition('prezent_doctrine_translatable.sonata.filter'));
